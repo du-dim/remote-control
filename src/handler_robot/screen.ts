bd8fb601104553a95aa32d8bdcc86b33ffa70830
du-dim <dim-dubovik@yandex.ru>
2022-06-25 13:46:38 +0300

    feat: swap red and blue channel

--- src/handler_robot/screen.ts
+++ src/handler_robot/screen.ts
@@ -6,8 +6,16 @@ export const screen = async () => {
   const y = robot.getMousePos().y;
   const size = 100;
   const bitmap = robot.screen.capture(x - size, y - size, size * 2, size * 2);
+  swapRedAndBlueChannel(bitmap);
   const image = new Jimp({ data: bitmap.image, width: bitmap.width, height: bitmap.height });
   const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
   const base64 = buffer.toString('base64');
   return base64;
+};
+
+const swapRedAndBlueChannel = (bmp: robot.Bitmap) => {
+  for (let i = 0; i < bmp.width * bmp.height * 4; i += 4) {
+    // swap red and blue channel
+    [bmp.image[i], bmp.image[i + 2]] = [bmp.image[i + 2], bmp.image[i]]; // red channel
+  }
 };
